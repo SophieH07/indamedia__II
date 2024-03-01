219483865773a34593891a36f0f9727cae1be9a7
Sophie <szaniszlozs7@gmail.com>
2024-03-01 15:34:01 +0100

    add warning

--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Email = () => {
   const [data, SetData] = useState(null);
+  const [accessGranted, setAccessGranted] = useState(false);
 
   useEffect(() => {
     const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
@@ -29,24 +30,47 @@ const Email = () => {
   return (
     <div className="text-center font-medium h-full">
       <p className="text-xl pb-2">XHR GET Request Example</p>
-      {data ? (
+      {!accessGranted ? (
         <div>
-          <p>Data Received:</p>
-          <ul className="flex flex-wrap justify-center">
-            {data.map((person) => (
-              <li
-                key={person.id}
-                className="block m-3 p-6 bg-sky-300 rounded-lg dark:bg-sky-800"
-              >
-                <p>First Name: {person.first_name}</p>
-                <p>Last Name: {person.last_name}</p>
-                <p>Email: {person.email}</p>
-              </li>
-            ))}
-          </ul>
+          <p>
+            First, because of CORS, to get the example, you have the request
+            temporary access to the demo server. Please visit this link
+          </p>
+          <p
+            className="cursor-pointer hover:underline"
+            onClick={() => setAccessGranted(true)}
+          >
+            <a
+              href="https://cors-anywhere.herokuapp.com/corsdemo"
+              target="_blank"
+            >
+              Access request for demo server
+            </a>
+          </p>
+          <p>then refresh the page and you will see the data.</p>
         </div>
       ) : (
-        <p className="pt-10">Loading...</p>
+        <div>
+          {data ? (
+            <div>
+              <p>Data Received:</p>
+              <ul className="flex flex-wrap justify-center">
+                {data.map((person) => (
+                  <li
+                    key={person.id}
+                    className="block m-3 p-6 bg-sky-300 rounded-lg dark:bg-sky-800"
+                  >
+                    <p>First Name: {person.first_name}</p>
+                    <p>Last Name: {person.last_name}</p>
+                    <p>Email: {person.email}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p className="pt-10">Loading...</p>
+          )}
+        </div>
       )}
     </div>
   );
